5dd956c3e8636d2350503023b80aa3b88d3f623c
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-02-14 15:44:26 +0100

    0.0.2

--- SecretAPI/SecretApi.cs
+++ SecretAPI/SecretApi.cs
@@ -23,7 +23,7 @@
         public override LoadPriority Priority => LoadPriority.Highest;
 
         /// <inheritdoc/>
-        public override Version Version => new(0, 0, 1);
+        public override Version Version => new(0, 0, 2);
 
         /// <inheritdoc/>
         public override Version RequiredApiVersion => new(1, 0, 0);
