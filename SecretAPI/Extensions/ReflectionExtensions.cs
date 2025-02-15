8bc8999fa6e98a3f753ccc90d66c4dc120f2df39
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-02-15 17:20:14 +0100

    ReflectionExtensions.CopyProperties

---
+++ SecretAPI/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace SecretAPI.Extensions
+{
+    using System.Reflection;
+
+    /// <summary>
+    /// Extensions for reflection.
+    /// </summary>
+    public static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Copies the properties.
+        /// </summary>
+        /// <param name="source">The source of the properties to copy.</param>
+        /// <param name="destination">Where to copy to.</param>
+        public static void CopyProperties(this object source, object destination)
+        {
+            foreach (PropertyInfo property in source.GetType().GetProperties())
+            {
+                destination.GetType().GetProperty(property.Name)?.SetValue(destination, property.GetValue(source));
+            }
+        }
+    }
+}
