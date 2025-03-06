8ca4d7c790d4ef56db9c3ecb61e1ed45d4c275a8
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-03-06 13:12:38 +0100

    Is<T>

--- SecretAPI/Extensions/ReflectionExtensions.cs
+++ SecretAPI/Extensions/ReflectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace SecretAPI.Extensions
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Reflection;
 
     /// <summary>
@@ -8,6 +9,24 @@
     /// </summary>
     public static class ReflectionExtensions
     {
+        /// <summary>
+        /// Attempts to cast an object to another type.
+        /// </summary>
+        /// <param name="obj">The object to attempt cast.</param>
+        /// <param name="value">The value it was cast to. Null if returned false.</param>
+        /// <typeparam name="T">The type to attempt casting to.</typeparam>
+        /// <returns>Whether the obj is T.</returns>
+        public static bool Is<T>(this object obj, [NotNullWhen(true)] out T? value)
+        {
+            value = default;
+
+            if (obj is not T val)
+                return false;
+
+            value = val;
+            return true;
+        }
+
         /// <summary>
         /// Copies the properties.
         /// </summary>
