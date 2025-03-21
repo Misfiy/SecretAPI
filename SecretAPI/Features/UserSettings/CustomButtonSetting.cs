f667a2901aa462c24614662e29427abfb96728b9
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-03-21 12:08:24 +0100

    Custom Button

--- SecretAPI/Features/UserSettings/CustomButtonSetting.cs
+++ SecretAPI/Features/UserSettings/CustomButtonSetting.cs
@@ -13,14 +13,14 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="CustomButtonSetting"/> class.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="label"></param>
-        /// <param name="header"></param>
-        /// <param name="onChanged"></param>
-        /// <param name="buttonText"></param>
-        /// <param name="holdTimeSeconds"></param>
-        /// <param name="hint"></param>
-        /// <param name="permissionCheck"></param>
+        /// <param name="id">The id of the button.</param>
+        /// <param name="label">The label of the button.</param>
+        /// <param name="header">The header to use as a base.</param>
+        /// <param name="onChanged">What to do on button using.</param>
+        /// <param name="buttonText">The button text.</param>
+        /// <param name="holdTimeSeconds">The time to hold.</param>
+        /// <param name="hint">The hint to show.</param>
+        /// <param name="permissionCheck">The permission check required.</param>
         public CustomButtonSetting(int? id, string label, CustomHeader header, Action<Player, CustomSetting> onChanged, string buttonText, float? holdTimeSeconds = null, string? hint = null, Predicate<Player>? permissionCheck = null)
             : base(new SSButton(id, label, buttonText, holdTimeSeconds, hint), header, onChanged, permissionCheck)
         {
