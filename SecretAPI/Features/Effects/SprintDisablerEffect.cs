c288867f3d41ffb5dd51fcde1ee0e17299357bd7
Nameless <85962933+Misfiy@users.noreply.github.com>
2025-03-21 20:23:32 +0100

    More Features/Effects (#14)
    * DisabledStaminaEffect
    
    * SprintDisablerEffect
    
    * <code>
    
    * Classified

---
+++ SecretAPI/Features/Effects/SprintDisablerEffect.cs
@@ -0,0 +1,19 @@
+﻿namespace SecretAPI.Features.Effects
+{
+    using PlayerRoles.FirstPersonControl;
+
+    /// <summary>
+    /// Effect that disables sprinting for a player.
+    /// </summary>
+    public class SprintDisablerEffect : CustomPlayerEffect, IStaminaModifier
+    {
+        /// <inheritdoc />
+        public bool StaminaModifierActive => IsEnabled;
+
+        /// <inheritdoc />
+        public bool SprintingDisabled => true;
+
+        /// <inheritdoc />
+        public override EffectClassification Classification => EffectClassification.Negative;
+    }
+}
