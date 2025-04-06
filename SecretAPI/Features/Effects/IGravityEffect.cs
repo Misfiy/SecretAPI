231b9dad08c707e46ef1f68f0629398d6fa5e745
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-04-06 15:08:58 +0200

    not good code but it works (maybe, dont test pls)

---
+++ SecretAPI/Features/Effects/IGravityEffect.cs
@@ -0,0 +1,15 @@
+﻿namespace SecretAPI.Features.Effects
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Handles an effect that affects gravity.
+    /// </summary>
+    public interface IGravityEffect
+    {
+        /// <summary>
+        /// Gets the gravity multiplier.
+        /// </summary>
+        public Vector3 GravityMultiplier { get; }
+    }
+}
---
+++ SecretAPI/Patches/Features/GravityEffectMultiplier.cs
@@ -0,0 +1,36 @@
+﻿namespace SecretAPI.Patches.Features
+{
+    using CustomPlayerEffects;
+    using HarmonyLib;
+    using PlayerRoles.FirstPersonControl;
+    using SecretAPI.Attribute;
+    using SecretAPI.Features.Effects;
+    using UnityEngine;
+
+    /// <summary>
+    /// Implements <see cref="IGravityEffect"/>.
+    /// </summary>
+    [HarmonyPatchCategory(nameof(CustomPlayerEffect))]
+    [HarmonyPatch(typeof(StatusEffectBase), nameof(StatusEffectBase.IntensityChanged))]
+    internal static class GravityEffectMultiplier
+    {
+#pragma warning disable SA1313
+        private static void Postfix(StatusEffectBase __instance)
+#pragma warning restore SA1313
+        {
+            Vector3 multi = Vector3.one;
+            foreach (StatusEffectBase effectBase in __instance.Hub.playerEffectsController.AllEffects)
+            {
+                if (!effectBase.IsEnabled || effectBase is not IGravityEffect gravityEffect)
+                    continue;
+
+                multi.x *= gravityEffect.GravityMultiplier.x;
+                multi.y *= gravityEffect.GravityMultiplier.y;
+                multi.z *= gravityEffect.GravityMultiplier.z;
+            }
+
+            if (__instance.Hub.roleManager.CurrentRole is IFpcRole currentRole)
+                currentRole.FpcModule.Motor.GravityController.Gravity = multi;
+        }
+    }
+}
