a45fd214cea2debe011b512e233d60d19e48bd8a
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-04-14 16:06:40 +0200

    examples

--- .github/README.md
+++ .github/README.md
@@ -4,8 +4,10 @@
 # Features
 - RoleExtensions: Extensions to help with RoleTypeId
 - RoomExtensions: Extensions related to rooms
+- HarmonyExtensions: Extensions to provide more utility to Harmony
 - CustomPlayerEffect: Create new status effects
 - IRegister: Handle auto registering certain plugin features
+- CustomSetting: SSSS wrappers providing some utility with permission checks 
 
 # Support
 * For any issues create an [Issue](https://github.com/Misfiy/SecretAPI/issues/new) or contact me on [Discord](https://discord.gg/RYzahv3vfC).
---
+++ SecretAPI.Examples/ExampleEntry.cs
@@ -0,0 +1,36 @@
+﻿namespace SecretAPI.Examples
+{
+    using System;
+    using LabApi.Loader.Features.Plugins;
+
+    /// <summary>
+    /// Defines the entry for the plugin.
+    /// </summary>
+    public class ExampleEntry : Plugin
+    {
+        /// <inheritdoc/>
+        public override string Name { get; } = "SecretAPI.Example";
+
+        /// <inheritdoc/>
+        public override string Description { get; } = "An example plugin";
+
+        /// <inheritdoc/>
+        public override string Author { get; } = "@misfiy";
+
+        /// <inheritdoc/>
+        public override Version Version { get; } = typeof(SecretApi).Assembly.GetName().Version;
+
+        /// <inheritdoc/>
+        public override Version RequiredApiVersion { get; } = new(LabApi.Features.LabApiProperties.CompiledVersion);
+
+        /// <inheritdoc/>
+        public override void Enable()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void Disable()
+        {
+        }
+    }
+}
