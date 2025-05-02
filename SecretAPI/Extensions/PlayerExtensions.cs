7a0f0224e559c2d93e6643d9a5f5ecd56fd38a46
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-05-02 14:51:55 +0200

    HasKeycardPermission

---
+++ SecretAPI/Extensions/PlayerExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace SecretAPI.Extensions
+{
+    using Interactables.Interobjects.DoorUtils;
+    using LabApi.Features.Wrappers;
+
+    /// <summary>
+    /// Extensions related to the player.
+    /// </summary>
+    public static class PlayerExtensions
+    {
+        /// <summary>
+        /// Checks whether a player has permission to access a <see cref="IDoorPermissionRequester"/>.
+        /// </summary>
+        /// <param name="player">The player to check.</param>
+        /// <param name="requester">The requester to check the player for permissions on.</param>
+        /// <returns>Whether a valid permission was found.</returns>
+        public static bool HasKeycardPermission(this Player player, IDoorPermissionRequester requester)
+        {
+            if (player.IsBypassEnabled)
+                return true;
+
+            if (player.RoleBase is IDoorPermissionProvider roleProvider && requester.PermissionsPolicy.CheckPermissions(roleProvider.GetPermissions(requester)))
+                return true;
+
+            foreach (Item? item in player.Items)
+            {
+                if (item?.Base is IDoorPermissionProvider itemProvider && requester.PermissionsPolicy.CheckPermissions(itemProvider.GetPermissions(requester)))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
