624ffe9136a93326cdcf5e8e381d6a4d6864c49c
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-05-18 00:27:59 +0200

    finally over.

--- SecretAPI/Features/CustomKeycards/CustomSite02KeycardInfo.cs
+++ SecretAPI/Features/CustomKeycards/CustomSite02KeycardInfo.cs
@@ -1,6 +1,7 @@
 ﻿namespace SecretAPI.Features.CustomKeycards
 {
     using LabApi.Features.Wrappers;
+    using UnityEngine;
 
     /// <summary>
     /// Handles info related to <see cref="ItemType.KeycardCustomSite02"/>.
@@ -10,9 +11,36 @@
         /// <inheritdoc />
         public override ItemType ItemType => ItemType.KeycardCustomSite02;
 
+        /// <summary>
+        /// Gets or sets the label of the card.
+        /// </summary>
+        public abstract string CardLabel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the holder of the card.
+        /// </summary>
+        public abstract string HolderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color of the label.
+        /// </summary>
+        public abstract Color LabelColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wear level.
+        /// </summary>
+        public abstract byte WearLevel { get; set; }
+
         /// <inheritdoc />
-        public override KeycardItem GiveKeycard(Player player)
-        {
-        }
+        public override KeycardItem GiveKeycard(Player player) => KeycardItem.CreateCustomKeycardSite02(
+            player,
+            ItemName,
+            HolderName,
+            CardLabel,
+            KeycardPermissions,
+            KeycardColor,
+            PermissionsColor,
+            LabelColor,
+            WearLevel)!;
     }
 }
--- SecretAPI/Features/CustomKeycards/CustomTaskForceKeycardInfo.cs
+++ SecretAPI/Features/CustomKeycards/CustomTaskForceKeycardInfo.cs
@@ -10,9 +10,30 @@
         /// <inheritdoc />
         public override ItemType ItemType => ItemType.KeycardCustomTaskForce;
 
+        /// <summary>
+        /// Gets or sets the name of the holder of the card.
+        /// </summary>
+        public abstract string HolderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the label of the serial.
+        /// </summary>
+        public abstract string SerialLabel { get; set; }
+
+        /// <summary>
+        /// Gets or sets rank index.
+        /// </summary>
+        public abstract int RankIndex { get; set; }
+
         /// <inheritdoc />
-        public override KeycardItem GiveKeycard(Player player)
-        {
-        }
+        public override KeycardItem GiveKeycard(Player player) => KeycardItem.CreateCustomKeycardTaskForce(
+            player,
+            ItemName,
+            HolderName,
+            KeycardPermissions,
+            KeycardColor,
+            PermissionsColor,
+            SerialLabel,
+            RankIndex)!;
     }
 }
