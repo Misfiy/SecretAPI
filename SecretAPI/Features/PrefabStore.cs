519b1033db297614d4f76a9169a9d856f428e3d6
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-05-18 12:30:35 +0200

    PrefabStore

---
+++ SecretAPI/Features/PrefabStore.cs
@@ -0,0 +1,36 @@
+﻿namespace SecretAPI.Features
+{
+    using System.Linq;
+    using Mirror;
+    using PlayerRoles.Ragdolls;
+    using UnityEngine;
+
+    /// <summary>
+    /// Handles the storing of a prefab.
+    /// </summary>
+    /// <typeparam name="TPrefab">The prefab to use.</typeparam>
+    public static class PrefabStore<TPrefab>
+    {
+        private static TPrefab? savedPrefab;
+
+        /// <summary>
+        /// Gets the <see cref="TPrefab"/> associated.
+        /// </summary>
+        public static TPrefab Prefab
+        {
+            get
+            {
+                if (savedPrefab != null)
+                    return savedPrefab;
+
+                foreach (GameObject gameObject in NetworkClient.prefabs.Values.Concat(RagdollManager.AllRagdollPrefabs.Select(r => r.gameObject)))
+                {
+                    if (gameObject.TryGetComponent(out savedPrefab))
+                        return savedPrefab!;
+                }
+
+                return default!;
+            }
+        }
+    }
+}
