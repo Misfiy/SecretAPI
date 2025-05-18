2255e7c1adb66830f2a9a7323f9a9d5cbd6457c2
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-05-18 15:37:11 +0200

    Mirror!!??!?!

---
+++ SecretAPI/Extensions/MirrorExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace SecretAPI.Extensions
+{
+    using System;
+    using LabApi.Features.Wrappers;
+    using Mirror;
+    using Respawning;
+
+    /// <summary>
+    /// Extensions related to Mirror.
+    /// </summary>
+    public static class MirrorExtensions
+    {
+        /// <summary>
+        /// Sends a fake cassie message to a player.
+        /// </summary>
+        /// <param name="target">The target to send the cassie message to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="isHeld">Whether the cassie is held.</param>
+        /// <param name="isNoisy">Whether the cassie is noisy.</param>
+        /// <param name="isSubtitles">Whether there is subtitles on the cassie.</param>
+        /// <param name="customSubtitles">The custom subtitles to use for the cassie.</param>
+        public static void SendFakeCassieMessage(
+            this Player target,
+            string message,
+            bool isHeld = false,
+            bool isNoisy = true,
+            bool isSubtitles = true,
+            string customSubtitles = "")
+        {
+            foreach (RespawnEffectsController allController in RespawnEffectsController.AllControllers)
+            {
+                SendFakeRpcMessage(target, allController, typeof(RespawnEffectsController), nameof(RespawnEffectsController.RpcCassieAnnouncement), message, isHeld, isNoisy, isSubtitles, customSubtitles);
+            }
+        }
+
+        /// <summary>
+        /// Send a fake rpc message to a player.
+        /// </summary>
+        /// <param name="target">The target to send the rpc to.</param>
+        /// <param name="behaviour"></param>
+        /// <param name="type"></param>
+        /// <param name="rpc"></param>
+        /// <param name="values"></param>
+        public static void SendFakeRpcMessage(this Player target, NetworkBehaviour behaviour, Type type, string rpc, params object[] values)
+        {
+            NetworkWriter writer = NetworkWriterPool.Get();
+
+            foreach (object obj in values)
+                writer.Write(obj);
+
+            RpcMessage rpcMessage = new()
+            {
+                netId = behaviour.netId,
+                componentIndex = behaviour.ComponentIndex,
+                functionHash = (ushort)$"{type.FullName}.{rpc}".GetStableHashCode(),
+                payload = writer.ToArraySegment(),
+            };
+
+            target.Connection.Send(rpcMessage);
+        }
+    }
+}
