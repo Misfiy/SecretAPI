ba7b32ca7c9e6ee7aba65a20f4cb2be022093561
Misfiy <85962933+Misfiy@users.noreply.github.com>
2025-08-06 19:06:50 +0200

    command example

---
+++ SecretAPI.Examples/Commands/ExampleExplodeCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace SecretAPI.Examples.Commands
+{
+    using LabApi.Features.Wrappers;
+    using SecretAPI.Attribute;
+    using SecretAPI.Features.Commands;
+
+    /// <summary>
+    /// An example of a <see cref="CustomCommand"/> that explodes a player.
+    /// </summary>
+    public class ExampleExplodeCommand : CustomCommand
+    {
+        /// <inheritdoc/>
+        public override string Command { get; } = "explode";
+
+        /// <inheritdoc/>
+        public override string[] Aliases { get; } = [];
+
+        /// <inheritdoc/>
+        public override string Description { get; } = "Explodes a player";
+
+        [ExecuteCommand]
+        private void Run(Player self, Player target)
+        {
+            TimedGrenadeProjectile.SpawnActive(target.Position, ItemType.GrenadeHE, self);
+        }
+    }
+}
